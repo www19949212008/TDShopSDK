4f01a7279eea346334a49c336a43ac04c0ad21f7
西兰花 <jony.chen@onedollartech.com>
2019-04-28 14:25:59 +0800

    firstcommit

---
+++ TDShopSDK.framework/Headers/TDShopSDK.h
@@ -0,0 +1,19 @@
+//
+//  TDShopSDK.h
+//  TDShopSDK
+//
+//  Created by user on 2019/4/9.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDShopSDK.
+FOUNDATION_EXPORT double TDShopSDKVersionNumber;
+
+//! Project version string for TDShopSDK.
+FOUNDATION_EXPORT const unsigned char TDShopSDKVersionString[];
+
+
+
+
